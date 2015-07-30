dc2e9ada1aebc89c887c848fc1752257d86e32c4
Michael Matranga <mike@dmgctrl.com>
2015-07-30 10:17:32 -0400

    Better naming.

--- SessionTwo/MikeMatranga/parser.rb
+++ SessionTwo/MikeMatranga/parser.rb
@@ -18,24 +18,24 @@ class Parser
   # given an ethnicity returns the 5 most popular baby names baby name, count
   def baby_names_by_ethnicity(ethnicity)
     parse_baby_names
-    @names_by_ethnicity[ethnicity].sort_by { |entry| entry.count }.reverse![0..4]
+    @entries_by_ethnicity[ethnicity].sort_by { |entry| entry.count }.reverse![0..4]
   end
 
   def ethnicity_stats_for_name(name)
     parse_baby_names
-    @ethinicity_count_by_name[name].sort_by { |entry| entry.count }.reverse!
+    @entries_by_name[name].sort_by { |entry| entry.count }.reverse!
   end
 
   private
 
   def parse_baby_names
-    @names_by_ethnicity = {}
-    @ethinicity_count_by_name = {}
+    @entries_by_ethnicity = {}
+    @entries_by_name = {}
 
     CSV.foreach(@file_path, col_sep: ',', quote_char: '"', headers: :first_row, converters: [:numeric], skip_blanks: true) do |row|
       name_entry = build_name_entry_from_row(row)
-      build_object(@names_by_ethnicity, name_entry.ethnicity, name_entry)
-      build_object(@ethinicity_count_by_name, name_entry.name, name_entry)
+      build_object(@entries_by_ethnicity, name_entry.ethnicity, name_entry)
+      build_object(@entries_by_name, name_entry.name, name_entry)
     end
   end
 
