3cfdaf1646ad80ed16e74051a67b32fa3ea13e58
Michael Matranga <mike@dmgctrl.com>
2015-07-29 20:09:24 -0400

    Grouping by ethnicity.

--- SessionTwo/MikeMatranga/app.rb
+++ SessionTwo/MikeMatranga/app.rb
@@ -3,13 +3,20 @@ require 'csv'
 file_path = "./data.csv"
 
 names = []
+names_by_ethnicity = {}
+
 CSV.foreach(file_path, col_sep: ',', quote_char: '"', headers: :first_row, converters: [:numeric], skip_blanks: true) do |row|
   name_entry = {}
   name_entry[:birth_year] = row['BRTH_YR']
-  name_entry[:ethnicity] = row['ETHCTY']
   name_entry[:name] = row['NAME']
   name_entry[:count] = row['CNT']
+
+  if names_by_ethnicity.has_key?(row['ETHCTY'])
+    names_by_ethnicity[row['ETHCTY']] << name_entry
+  else
+    names_by_ethnicity[row['ETHCTY']] = [name_entry]
+  end
   names << name_entry
 end
 
-puts names
+puts names_by_ethnicity['ASIAN AND PACIFIC ISLANDER'].sort_by { |entry| entry[:count] }
