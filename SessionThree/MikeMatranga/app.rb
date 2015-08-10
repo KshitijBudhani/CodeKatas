6c38a8cc60dedd5c4f7a486d90499660458af264
Michael Matranga <mike@dmgctrl.com>
2015-08-10 16:52:14 -0400

    Moved all pieces into vars.

--- SessionThree/MikeMatranga/app.rb
+++ SessionThree/MikeMatranga/app.rb
@@ -4,15 +4,16 @@ require 'openssl'
 require 'json'
 
 o_auth_token = '6478e1e90b7c64fb79e2737df613c63c7d735fad'
-uri = URI.parse("https://api.github.com/users/matrangam/orgs?access_token=#{o_auth_token}")
+authentication = "access_token=#{o_auth_token}"
+username = "matrangam" #XXX: To come from the argzzzzzzzz
+repos_url = "https://api.github.com/users/#{username}/repos"
 
+uri = URI.parse("#{repos_url}?#{authentication}")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-# 6478e1e90b7c64fb79e2737df613c63c7d735fad
 request = Net::HTTP::Get.new(uri.request_uri)
-# eq.basic_auth 'user', 'pass'
 response = http.request(request)
 puts response.body
 
